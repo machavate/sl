d96dbcda1dd5ae8cdc8f2737bf6a7c682e4b11a1
islands <osmaneandrademachavate@gmail.com>
2022-06-04 18:40:24 +0200

    updating informing page and adding the 404 page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,8 +1,12 @@
+import { Container, Building } from "./styles"
+
 export default function Home() {
     return (
-        <div>
-            <h1>SL Taxi services</h1>
-            <p>It is being built for you!</p>
-        </div>
+        <Container>
+            <Building>
+                <h1>SL Taxi services</h1>
+                <p>It is being built for you!</p>
+            </Building>
+        </Container>
     )
 }
